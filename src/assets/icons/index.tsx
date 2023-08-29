8367354a7d6a7a01204ab317797d33917b9f45b8
goktugs <gokojapan5@gmail.com>
2023-08-29 19:43:45 +0300

    feat: add footer

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,9 @@
+import InstagramIcon from "./InstaIcon";
+
+import PinterestIcon from "./PinterestIcon";
+
+import RightIcon from "./RightIcon";
+
+import TwitterIcon from "./TwitterIcon";
+
+export { InstagramIcon, RightIcon, TwitterIcon, PinterestIcon };
