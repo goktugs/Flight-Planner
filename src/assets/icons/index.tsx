0b15e067a7f65a7f389dc034bcbd82da49d566d9
goktugs <gokojapan5@gmail.com>
2023-08-31 02:06:19 +0300

    feat: add main ui

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -8,12 +8,13 @@ import TwitterIcon from "./TwitterIcon";
 
 import UserIcon from "./UserIcon";
 import SearchIcon from "./SearchIcon";
-
+import PlaneIcon from "./PlaneIcon";
 export {
   InstagramIcon,
   RightIcon,
   TwitterIcon,
   PinterestIcon,
   UserIcon,
+  PlaneIcon,
   SearchIcon,
 };
