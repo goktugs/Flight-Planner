114924696cc3c9d586c15cfbe7765f45f5230723
goktugs <gokojapan5@gmail.com>
2023-09-03 02:13:46 +0300

    wip: add toasts

--- src/store/flightSlice.ts
+++ src/store/flightSlice.ts
@@ -1,16 +1,11 @@
 import { IAirport } from "@/types/fligthSearch";
 import { create } from "zustand";
-import { persist, createJSONStorage } from "zustand/middleware";
 
 export const useFlightStore = create(
-  persist(
-    (set, get) => ({
-      flights: [],
-      setFlights: (flights: IAirport) => set({ flights }),
-    }),
-    {
-      name: "flight-storage",
-      storage: createJSONStorage(() => localStorage),
-    }
-  )
+  (
+    set
+  ): { airports: IAirport[]; setAirports: (airports: IAirport[]) => void } => ({
+    airports: [],
+    setAirports: (airports: IAirport[]) => set({ airports }),
+  })
 );
